9f36984f6efc706008008c70b56470cf7bf35d7c
SergeySmirnovJava <sergej.smirnov1998@yandex.ru>
2021-08-24 04:57:54 +0300

    init commit

---
+++ src/test/java/com/exchange_rate/ExchangeRateBot/ExchangeRateBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange_rate.ExchangeRateBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
