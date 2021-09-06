15d46cac97d9323b9f5fa7fe60bb305fdc9d81b7
SergeySmirnovJava <sergej.smirnov1998@yandex.ru>
2021-09-06 23:34:45 +0300

    refactor controller class

--- src/main/java/com/exchange_rate/ExchangeRateBot/controllers/BaseController.java
+++ src/main/java/com/exchange_rate/ExchangeRateBot/controllers/BaseController.java
@@ -4,6 +4,7 @@ import com.exchange_rate.ExchangeRateBot.dto.CurrencyDto;
 import com.exchange_rate.ExchangeRateBot.dto.DataToCurrency;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,9 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,18 +29,17 @@ public class BaseController {
                 produces = MediaType.APPLICATION_JSON_VALUE)
     public CurrencyDto getExchange(@PathVariable("target") String target,
                                    @PathVariable("base") String base,
-                                   @RequestParam(value = "day", defaultValue = "day") String day,
+                                   @RequestParam(value = "date") String date,
                                    @RequestParam(value = "amount", defaultValue = "amount") String amount) throws JsonProcessingException {
-
+        new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
         String typeQuery = String.format("/M.%s.%s.SP00.A",target, base);
         UriComponentsBuilder uriComponents = UriComponentsBuilder.newInstance();
         uriComponents.scheme("https")
-                .host("sdw-wsrest.ecb.europa.eu")
-                .path("/service/data/EXR")
-                .path(typeQuery)
-                .build();
-
-        System.out.println(uriComponents.toUriString());
+                        .host("sdw-wsrest.ecb.europa.eu")
+                        .path("/service/data/EXR")
+                        .path(typeQuery)
+                        .queryParam("startPeriod", date)
+                        .build();
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
@@ -45,35 +48,7 @@ public class BaseController {
 
         ResponseEntity<String> responseEntity = restTemplate.exchange(uriComponents.toUriString(), HttpMethod.GET, entity, String.class);
         DataToCurrency dataToCurrency = new DataToCurrency(responseEntity.getBody());
-        System.out.println(dataToCurrency.getExchangeResult());
-         return dataToCurrency.getExchangeResult();
+        return dataToCurrency.getExchangeResult();
     }
 
-
-
-
-
-    @GetMapping("/test")
-        public CurrencyDto test(@RequestParam(value = "name", defaultValue = "World") String name){
-            RestTemplate restTemplate = new RestTemplate();
-            HttpHeaders headers = new HttpHeaders();
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-            HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-            ResponseEntity<String> responseEntity = restTemplate.exchange("https://sdw-wsrest.ecb.europa.eu/service/data/EXR/M.USD.EUR.SP00.A",
-                                                            HttpMethod.GET, entity,String.class);
-            //CurrencyDto currencyDto;
-            //currencyDto = (responseEntity.);
-            System.out.println(responseEntity.getBody());
-                    //restTemplate.getForObject("https://graph.facebook.com/pivotalsoftware",entity ,CurrencyDto.class);
-            //System.out.println(currencyDto.getHeader());
-            //System.out.println(currencyDto.getError());
-            return null;
-        }
-
-        @GetMapping("/hello")
-        public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-            return String.format("Hello %s!", name);
-        }
 }
